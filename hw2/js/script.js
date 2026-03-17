f36f62c6bf17cc610475d54fb5ff615cfb759618
Rawley Lizarraga <rawleylizarraga@gmail.com>
2026-03-16 18:05:38 -0700

    hw2 part 13

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -3,6 +3,7 @@ document.querySelector("button").addEventListener("click", gradeQuiz)
 
 // Global Variables
 let score = 0;
+let attempts = localStorage.getItem("total_attempts");
 
 let validationFdbk = document.querySelector("#validationFdbk");
 
@@ -13,7 +14,7 @@ displayQ4Choices();
 function displayQ4Choices() {
     let q4ChoicesArray = ["Maine", "Rhode Island", "Maryland", "Delaware"];
     q4ChoicesArray = _.shuffle(q4ChoicesArray);
-    
+
     for (let i = 0; i < q4ChoicesArray.length; i++) {
         document.querySelector("#q4Choices").innerHTML += ` <input type="radio" name="q4" id="${q4ChoicesArray[i]}" value="${q4ChoicesArray[i]}"> <label for="${q4ChoicesArray[i]}"> ${q4ChoicesArray[i]} </label>`
     }
@@ -89,4 +90,6 @@ function gradeQuiz() {
 
 
     document.querySelector("#totalScore").innerHTML = `Total Score ${score}`;
+    document.querySelector("#totalAttempts").innerHTML = `Total Attempts: ${++attempts}`;
+    localStorage.setItem("total_attempts", attempts);
 }
