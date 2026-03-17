cced63950429fca8db2f4ad65b3e6b9ca3812370
Rawley Lizarraga <rawleylizarraga@gmail.com>
2026-03-16 17:30:20 -0700

    h2 parts 7-8

--- hw2/js/script.js
+++ hw2/js/script.js
@@ -2,9 +2,9 @@
 document.querySelector("button").addEventListener("click", gradeQuiz)
 
 // Global Variables
+let score = 0;
+
 let validationFdbk = document.querySelector("#validationFdbk");
-let q1Feedback = document.querySelector("#q1Feedback");
-let markImg1 = document.querySelector("#markImg1");
 
 
 // Functions
@@ -18,6 +18,19 @@ function isFormValid() {
     return isValid;
 }
 
+function rightAnswer(index) {
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Correct!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-success text-white";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
+    score += 20;
+}
+
+function wrongAnswer(index) {
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Incorrect!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-warning text-white";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/xmark.png' alt='xmark'>";
+}
+
 function gradeQuiz() {
     console.log("Grading quiz...");
     validationFdbk.innerHTML = "";
@@ -26,22 +39,27 @@ function gradeQuiz() {
         return;
     }
 
-    let score = 0;
+    score = 0;
 
     let q1Response = document.querySelector("#q1").value.toLowerCase();
+    let q2Response = document.querySelector("#q2").value;
     console.log(q1Response);
+    console.log(q2Response);
 
     // grade question 1
     if (q1Response == "sacramento") {
-        q1Feedback.innerHTML = "Correct!";
-        q1Feedback.className = "bg-success text-white";
-        markImg1.innerHTML = "<img src='img/checkmark.png' alt='Checkmark'";
-        score += 20;
+        rightAnswer(1);
     } else {
-        q1Feedback.innerHTML = "Incorrect!";
-        q1Feedback.className = "bg-warning text-white";
-        markImg1.innerHTML = "<img src='img/xmark.png' alt='xmark'";
+        wrongAnswer(1);
     }
+
+    // grade question 2
+    if (q2Response == "mo") {
+        rightAnswer(2);
+    } else {
+        wrongAnswer(2);
+    }
+
 
     document.querySelector("#totalScore").innerHTML = `Total Score ${score}`;
 }
